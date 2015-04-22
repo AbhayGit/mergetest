82004dd55248b90f8010fce206a769347f8824c8
CGTAbhay <abhay@cgt.co.in>
2015-04-22 16:51:36 +0530

    updates in php

--- test.php
+++ test.php
@@ -1,1 +1,16 @@
-<?php function first(){}?>
+<?php 
+	function first(){
+
+		echo 'ascascascascascascascsc';
+	}
+
+	function second(){
+
+		echo '1234567890-=]';
+	}
+
+	function third(){
+
+		echo '12345678890-';
+	}	
+?>
